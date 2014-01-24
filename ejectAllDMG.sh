4afa4fd16c8c0c9792b3338a68dbd2f4bb6b216c
Carl Henderson <carl.henderson@chernoffnewman.com>
2014-01-24 11:00:16 -0500

    added script

---
+++ ejectAllDMG.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VOLUMES=/Volumes/*
+MATCH='Disk Image'
+
+# Loop through all mounted volumes
+for v in $VOLUMES
+do
+	
+	# Check if volume is a disk image
+	PROTOCOL=$(diskutil info $v | grep "Protocol:" | sed 's/.*Protocol:[[:space:]]*//')
+	if [ "$PROTOCOL" == "$MATCH" ]; then
+
+		# if volume is a disk image, eject it
+		diskutil eject $v
+		
+		# send a notification using terminal-notifier
+		terminal-notifier -message "$v ejected"
+	
+	fi
+
+done
